4ef5ae3a3b22afb2554d301a3bbe88908d985340
josicLacbr <85517758+josicLacbr@users.noreply.github.com>
2022-11-06 21:47:32 -0500

    Added pyspark_recommendations.py

---
+++ pyspark_recommendations.py
@@ -0,0 +1,16 @@
+import sys
+ 
+from pyspark import SparkContext
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        # create Spark context with necessary configuration
+        sc = SparkContext("local","PySpark Social Network Problem")
+
+        # read data from text file and split each line into a list
+        friends = sc.textFile(sys.argv[1]).map(lambda line: line.split("\t"))
+  
+        friends.saveAsTextFile(sys.argv[2])
+
+    else:
+        print("Usage: {} <input> <output>".format(sys.argv[0]), file=sys.stderr)
