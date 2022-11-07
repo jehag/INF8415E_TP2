293346a22e963da0d0d02ee2a892bf7903415b82
jehag <jeremy.hage@polymtl.ca>
2022-11-06 21:30:54 -0500

    added command to run spark

--- azure.sh
+++ azure.sh
@@ -54,3 +54,5 @@ sudo mv spark-3.3.1-bin-hadoop3 /opt/spark
 export SPARK_HOME=/opt/spark
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 export PYSPARK_PYTHON=/usr/bin/python3
+
+spark-submit pyspark_wordcount.py input output
