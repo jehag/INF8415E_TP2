4b1b508cba5f5e5579714095b0c92e75c5ab25d6
jehag <jeremy.hage@polymtl.ca>
2022-11-06 21:28:51 -0500

    added argv in pyspark wordcount

--- pyspark_wordcount.py
+++ pyspark_wordcount.py
@@ -3,14 +3,17 @@ import sys
 from pyspark import SparkContext
  
 if __name__ == "__main__":
-    # create Spark context with necessary configuration
-    sc = SparkContext("local","PySpark Word Count Exmaple")
+    if len(sys.argv) == 3:
+        # create Spark context with necessary configuration
+        sc = SparkContext("local","PySpark Word Count Exmaple")
 
-    # read data from text file and split each line into words
-    words = sc.textFile("input").flatMap(lambda line: line.split(" "))
+        # read data from text file and split each line into words
+        words = sc.textFile(sys.argv[1]).flatMap(lambda line: line.split(" "))
 
-    # count the occurrence of each word
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a +b)
+        # count the occurrence of each word
+        wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
-    # save the counts to output
-    wordCounts.saveAsTextFile("output")
+        # save the counts to output
+        wordCounts.saveAsTextFile(sys.argv[2])
+    else:
+        print("Usage: {} <input> <output>".format(sys.argv[0]), file=sys.stderr)
